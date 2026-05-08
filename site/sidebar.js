6c39cf1cce93c363b996692e5fc3461685002962
bitplugg <zxckillka.off@gmail.com>
2026-05-08 06:00:54 +0300

    clean URLs: folder-based structure (no .html in paths)

--- site/sidebar.js
+++ site/sidebar.js
@@ -16,28 +16,29 @@ var ARTICLES = [
 var LANG = document.documentElement.lang;
 
 function buildSidebar() {
-  var current = location.pathname.split("/").pop();
-  var html = '<div class="sidebar-header"><a href="../index.html">nixos-pack</a></div><nav><ol>';
+  var parts = location.pathname.replace(/\/$/, "").split("/");
+  var current = parts[parts.length - 1] || ARTICLES[0].id;
+  var html = '<div class="sidebar-header"><a href="../../index.html">nixos-pack</a></div><nav><ol>';
   var activeIdx = -1;
   for (var i = 0; i < ARTICLES.length; i++) {
     var a = ARTICLES[i];
     var name = a[LANG] || a.en;
-    var file = a.id + ".html";
-    var active = file === current ? ' class="active"' : '';
-    if (file === current) activeIdx = i;
-    html += '<li' + active + '><a href="' + file + '">' + (i + 1) + '. ' + name + '</a></li>';
+    var active = a.id === current ? ' class="active"' : "";
+    if (a.id === current) activeIdx = i;
+    html += '<li' + active + '><a href="../' + a.id + '/">' + (i + 1) + ". " + name + "</a></li>";
   }
-  html += '</ol></nav>';
+  html += "</ol></nav>";
   if (activeIdx !== -1) {
-    var prev = activeIdx > 0 ? ARTICLES[activeIdx - 1].id + ".html" : null;
-    var next = activeIdx < ARTICLES.length - 1 ? ARTICLES[activeIdx + 1].id + ".html" : null;
+    var prev = activeIdx > 0 ? "../" + ARTICLES[activeIdx - 1].id + "/" : null;
+    var next = activeIdx < ARTICLES.length - 1 ? "../" + ARTICLES[activeIdx + 1].id + "/" : null;
     var pLabel = LANG === "ru" ? "Назад" : "Prev";
     var nLabel = LANG === "ru" ? "Вперёд" : "Next";
     html += '<div class="sidebar-bottom">';
-    if (prev) html += '<a href="' + prev + '" class="nav-prev">← ' + pLabel + '</a>';
-    if (next) html += '<a href="' + next + '" class="nav-next">' + nLabel + ' →</a>';
-    html += '<a href="../' + (LANG === "ru" ? "en" : "ru") + '/' + current + '" class="lang-switch">' + (LANG === "ru" ? "EN" : "RU") + '</a>';
-    html += '</div>';
+    if (prev) html += '<a href="' + prev + '" class="nav-prev">← ' + pLabel + "</a>";
+    if (next) html += '<a href="' + next + '" class="nav-next">' + nLabel + " →</a>";
+    var other = LANG === "ru" ? "en" : "ru";
+    html += '<a href="../../' + other + "/" + current + '/" class="lang-switch">' + (LANG === "ru" ? "EN" : "RU") + "</a>";
+    html += "</div>";
   }
   document.getElementById("sidebar").innerHTML = html;
 }
